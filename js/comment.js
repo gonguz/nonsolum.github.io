3690963201726815b016dd985bd130c4eb576402
Gonzalo Guzman <gonzaguz@ucm.es>
2017-05-27 13:02:16 +0200

    Añadidas imagenes y texto

---
+++ js/comment.js
@@ -0,0 +1,6 @@
+(function(){
+if(!window.hcb_user){hcb_user={};} (function(){var s =document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";
+s.setAttribute("type","text/javascript");
+s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24O.tO%2Fn20ihGvf1bCG4BMf."+"&opts=16862&num=3&ts=1495882282954");
+if (typeof s !="undefined"){ document.getElementsByTagName("head")[0].appendChild(s); }
+})();
