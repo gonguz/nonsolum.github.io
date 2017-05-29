32d69f5a0fbab44bd3d8af5703dc0c1fc8629de6
Gonzalo Guzman <gonzaguz@ucm.es>
2017-05-30 00:21:59 +0200

    Solo falta instalador y video, #durocaminoacardiff

--- js/comment.js
+++ js/comment.js
@@ -6,3 +6,413 @@ s.setAttribute("type","text/javascript");
 s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24O.tO%2Fn20ihGvf1bCG4BMf."+"&opts=16862&num=3&ts=1495882282954");
 if (typeof s !="undefined"){ document.getElementsByTagName("head")[0].appendChild(s); }
 })();*/
+
+/*<div class="mda-class">
+  <strong>ESTÉTICA:</strong>
+  <p>
+    El juego está ambientado en Mount Horeb, un pequeño pueblo Estadounidense,
+     cuya principal industria y fuente de ingresos es la ferroviaria.
+     La éstetica es Western y medieval muy cercana al siglo XIX, etapa de la Revolución Industrial.
+  </p>
+</div>*/
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NonSolum: A Serious-Train project</title>
+		<link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
+		<link rel="stylesheet" href="./styles/main.css" />
+		<link rel="stylesheet" href="./styles/header.css" />
+<!-- end www.htmlcommentbox.com -->
+	<style>
+	#PosImagen{position:relative; left: 150px;}
+	#setPosLeft{position: relative; left: 120px;}
+	#setPosRight{position: relative; left: 170px;}
+	#slenderPos{position: relative; left: 250px; margin: 20px 8px;}
+	#basicoPos{position: relative; left: 425px; top: 40px; margin: 20px 8px;}
+	</style>
+	<link rel="shortcut icon" href="./img/icon2.png"/>
+	<body>
+		<button id="myBtn" title="Go to top">Top</button>
+    <header>
+      <ul class="navbar">
+				<li class="navbar-link">
+					<a href="#history"><span>Historia</span></a>
+				</li>
+				<li class="navbar-link">
+					<a href="#game"><span>Nuestro Juego</span></a>
+				</li>
+				<li class="navbar-logo">
+					<img src="./img/logo.png" alt="Non solum logo">
+				</li>
+				<li class="navbar-link">
+					<a href="#team"><span>Serious Train Team</span></a>
+				</li>
+				<li class="navbar-link">
+					<a href="#footer"><span>Contacto</span></a>
+				</li>
+			</ul>
+    </header>
+  	<div>
+  		<section>
+				<h1><a href="https://github.com/borjacano97/Serious-Train" class="link-h1"><font color="FFF">Non Solum</font></a></h1>
+  			<img class="main-image" id="cloud-scroll">
+				<img class="tren-Image" src="./img/tren.png">
+</iframe>
+  		</section>
+  		<section class="correct-position">
+  			<h2 id ="history">Historia</h2>
+				<div class="history-container divided-container">
+					<div class="history-image">
+					</div>
+					<div class="top-text">
+						<p>
+							La vida antes del mayor desastre epidémico de la humanidad era de lo más tranquila. En concreto, esta tranquilidad resaltaba en Mount Hareb, una ciudad caracterizada por ser el epicentro de la industrialización, y sobre todo por su industria ferroviaria. A ella, acudía todo aquel que quería cumplir sus sueños, pues había grandes oportunidades para prosperar. Una de estas personas era Sam McConaughey, un granjero estadounidense hastiado del campo, el cuál quería emprender
+							un viaje en ferrocarril que le llevase a descubrir los lugares más inhóspitos del territorio americano. Sam pensó que la vida al final le daba una oportunidad, sin embargo la peste llegó inesperadamente al país...
+						</p>
+					</div>
+				</div>
+				<div class ="bottom-text">
+					Antes de que nadie pudiese hacer nada, los muertos vivientes plagaban las calles, sembrando el pánico y terror de la gente. Sam no tuvo otra opción que hacer frente a la terrible situación. El destino de Sam ya está escrito, solo tú serás capaz de cambiarlo. ¿Te atreves?.
+				</div>
+  		</section>
+  		<section class="correct-position">
+  			<h2 id="game">Nuestro Juego</h2>
+				<iframe  src="https://www.youtube.com/embed/0LtkwQyPHZw" class="video-responsive" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+				<div class="list-container">
+					<ul class="navbar">
+						<li class="our-game-item active" id="mda">
+							<span>MDA</span>
+						</li>
+						<li class="our-game-item" id="vagones">
+							<span>Vagones</span>
+						</li>
+						<li class="our-game-item" id="armas">
+							<span>Armas</span>
+						</li>
+					</ul>
+				</div>
+				<div id="mda-content">
+					<div class="mda-class">
+						<strong>MECÁNICAS:</strong>
+						<p>
+							En "NON SOLUM" controlarás a Sam McConaughey. Tu objetivo será el de limpiar Mount Horeb de la terrible plaga que asola la ciudad. Para ello tendrás que deshacerte de numerosas oleadas de zombies
+							los cuáles te someterán a inmensas dificultades. Para conseguirlo, dispondrás de armas y vagones adherentes que te harán tal dichosa tarea mucho más sencilla. Si estás preparado embarcate en el "Ubi Trinus".
+							A pesar de que muchos zombies te pondrán en dificultades, otros te ayudarán en esta aventura. Es el caso del Slender.
+						</p>
+					</div>
+					<div>
+						<img  src="./img/slender.png" alt="Jose Foto" id="slenderPos"/>
+						<img  src="./img/basico.png" alt="Jose Foto" id="basicoPos"/>
+					</div>
+					<div class="mda-class">
+						<strong>DINÁMICAS:</strong>
+						<p>
+							En el juego podrás disfrutar de tres modos de juego: <strong>En primer lugar, el modo Historia</strong> en el cuál guiarás a nuestro protagonista Sam en su ardua tarea de salvar al país de la epidemia que lo asola.
+							En este modo tendrás la posibilidad de acceder a la tienda. En ella podrás adquirir vagones que colocarás en tu tren y armas. Cada objeto tiene su precio. El dinero se conseguirá matando a los enemigos.
+						</p>
+						<div class="team-image">
+							<img src="./img/tienda.png"/>
+						</div>
+						<p>
+						<strong>En segundo lugar encontramos el modo supervivencia</strong> en el que tu objetivo será aguantar el mayor número posible de oleadas de enemigos. A medida que vayas pasando cada ronda,
+						Sam dispondrá de nuevas armas. Adéntrate en el modo Supervivencia y conoce el lado más frenético de "NON SOLUM".<br />
+						<br />
+						<strong>En último lugar, el modo Extremo</strong>. En este modo, Sam llevará el arma más potente que es posible comprar en la tienda. La cantidad de enemigos por ronda superará casi exponencialmente la cantidad de zombies por oleada en el modo Supervivencia.
+						Sin embargo, a pesar de las inmensas oleadas de zombies no podrás sentirte débil, ya que a medida que avances se colocarán vagones que permitirán arreglar tu tren.
+						</p>
+
+						<div class="mda-class">
+							<strong>MECÁNICAS:</strong>
+							<p>
+								En "NON SOLUM" controlarás a Sam McConaughey. Tu objetivo será el de limpiar Mount Horeb de la terrible plaga que asola la ciudad. Para ello tendrás que deshacerte de numerosas oleadas de zombies
+								los cuáles te someterán a inmensas dificultades. Para conseguirlo, dispondrás de armas y vagones adherentes que te harán tal dichosa tarea mucho más sencilla. Si estás preparado embarcate en el "Ubi Trinus".
+								A pesar de que muchos zombies te pondrán en dificultades, otros te ayudarán en esta aventura. Es el caso del Slender.
+							</p>
+						</div>
+					</div>
+					<div class="team-image">
+						<img src="./img/trenFull.png"/>
+					</div>
+
+				</div>
+				<div id="vagones-content" class="hidden">
+					<div class="team-descriptions">
+						<div class="divided-container">
+							<div id="setPosLeft">
+								<img  src="./img/vagon torreta.png" alt="Puma Foto"/>
+							</div>
+							<div>
+								<h3>Vagón Automático</h3>
+								<p>
+									Este vagón va a ser uno de tus mejores amigos durante la aventura. Disparará por tí cuando estés en apuros.
+								</p>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div>
+								<h3>
+									Vagón Escudo
+								</h3>
+								<p>
+									"Este vagón es imprescindible". Tal y como dice Alejandro, este vagón es muy importante para sobrevivir. Sin embargo, ¿sabrías
+									colocarlo correctamente?.
+								</p>
+							</div>
+							<div class="image-class" id="PosImagen">
+								<img  src="./img/vagon escudo.png" alt="Alex Foto"/>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div class="image-class" id="setPosLeft">
+								<img  src="./img/vagon laser.png" alt="Jose Foto"/>
+							</div>
+							<div>
+								<h3>
+									Vagón Laser
+								</h3>
+								<p>
+									Quizá uno de los vagones más infravalorados. A pesar de esto, yo te puedo decir que te salvará el cuello en muchas ocasiones.
+								</p>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div>
+								<h3>
+									Vagón Succionador
+								</h3>
+								<p>
+									El vagón favorito del equipo de desarrollo. Gracias a él, las pruebas de balanceo fueron más sencillas. Pruebalo y verás.
+								</p>
+							</div>
+							<div class="image-class" id="PosImagen">
+								<img  src="./img/vagon succionador.png" alt="Toscana Foto"/>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div class="image-class" id="setPosLeft">
+								<img  src="./img/vagon regenerador.png" alt="Andres Foto"/>
+							</div>
+							<div>
+								<h3>
+									Vagón Recuperador
+								</h3>
+								<p>
+									Su propio nombre lo dice, no hay mayor misterio. Por si acaso, este vagón recupera la vida del tren siempre y cuando estés encima de él.
+								</p>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div>
+								<h3>
+									Vagón Lanzallamas
+								</h3>
+								<p>
+									Con este vagón podrás quemar las cabelleras de los enemigos. Disfrutarás viendo como arden!.
+								</p>
+							</div>
+							<div class="image-class" id="PosImagen">
+								<img  src="./img/vagon lanzallamas.png" alt="Rober Foto"/>
+							</div>
+						</div>
+					</div>
+					</div>
+				<div id="armas-content" class="hidden">
+					<div class="team-descriptions">
+						<div class="divided-container">
+							<div id="setPosLeft">
+								<img  src="./img/revolver bueno.png" alt="Puma Foto" width="250" height="100"/>
+							</div>
+							<div>
+								<h3 id="setPosRight">REVÓLVER</h3>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div>
+								<h3 id="setPosLeft">
+									ESCOPETA
+								</h3>
+							</div>
+							<div class="image-class" id="PosImagen">
+								<img  src="./img/escopeta.png" alt="Alex Foto" width="210" height="70"/>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div class="image-class" id="setPosLeft">
+								<img  src="./img/francoDictador.png" alt="Jose Foto"/>
+							</div>
+							<div>
+								<h3 id="setPosRight">
+									FRANCOTIRADOR
+								</h3>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div>
+								<h3 id="setPosLeft">
+									METRALLETA
+								</h3>
+							</div>
+							<div class="image-class" id="PosImagen">
+								<img  src="./img/metralleta.png" alt="Toscana Foto"/>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div class="image-class" id="setPosLeft">
+								<img  src="./img/minigun.png" alt="Andres Foto"/>
+							</div>
+							<div>
+								<h3 id="setPosRight">
+									MINIGUN
+								</h3>
+							</div>
+						</div>
+						<div class="divided-container">
+							<div>
+								<h3 id="setPosLeft">
+									CAÑÓN
+								</h3>
+							</div>
+							<div class="image-class" id="PosImagen">
+								<img  src="./img/cañonazo.png" alt="Rober Foto"/>
+							</div>
+						</div>
+					</div>
+				</div>
+  		</section>
+  		<section class="correct-position">
+  			<h2 id="team" >Serious Train Team</h2>
+				<div class="team-image">
+					<img  src="./img/equipoNS.jpeg" alt="Imagen Equipo">
+				</div>
+				<div class="team-descriptions">
+					<div class="divided-container">
+						<div>
+							<img  src="./img/puma2.jpg" alt="Puma Foto" class="enmarcada"/>
+						</div>
+						<div>
+							<h3>Puma López Acero</h3>
+							<p>
+								"Soy todo un fiera, no se me resiste ninguna presa. Soy carnívoro, aunque de vez en cuando le doy al pescado, un saludo."
+							</p>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div>
+							<h3>
+								Alex Ortega Álvarez
+							</h3>
+							<p>
+								"Soy el master programmer, cualquier duda me preguntais. Además, cuando quieras me llamas y nos echamos unas magic o un tenis, valgo para todo, un saludo."
+							</p>
+						</div>
+						<div class="image-class" id="PosImagen">
+							<img  src="./img/alex.jpeg" alt="Alex Foto" class="enmarcada"/>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div class="image-class">
+							<img  src="./img/jose.jpeg" alt="Jose Foto" class="enmarcada"/>
+						</div>
+						<div>
+							<h3>
+								José Morales Jaurrieta
+							</h3>
+							<p>
+								"Técnico experto en creación y diseño de vagones. Presentador oficial de Serious Train. El resto lo define mi foto, un saludo."
+							</p>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div>
+							<h3>
+								Cesar Toscana Arroyo
+							</h3>
+							<p>
+								"Extremo defensor de los personajes de color en los videojuegos, y de todo aquello que sea bonito. Soy de Santander, mi plato favorito es la anchoa del Cantábrico, un saludo."
+							</p>
+						</div>
+						<div class="image-class" id="PosImagen">
+							<img  src="./img/toscana.jpeg" alt="Toscana Foto" class="enmarcada"/>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div class="image-class">
+							<img  src="./img/andres.jpeg" alt="Andres Foto" class="enmarcada"/>
+						</div>
+						<div>
+							<h3>
+								Andrés Puente Rodríguez
+							</h3>
+							<p>
+								"Historiador de Serious-Train. También soy Boy-Scout de vez en cuando. Además le doy al boxeo. Cuando quieras te reto Alex, un saludo."
+							</p>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div>
+							<h3>
+								Roberto Alcázar Morales
+							</h3>
+							<p>
+								"Encargado de las animaciones y presentaciones en este proyecto. Soy un gran amante de la cerveza, de hecho, puede que ahora me esté tomando una, un saludo."
+							</p>
+						</div>
+						<div class="image-class" id="PosImagen">
+							<img  src="./img/rober.jpeg" alt="Rober Foto" class="enmarcada"/>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div class="image-class">
+							<img  src="./img/gonzalo.jpg" alt="Gonzalo Foto" class="enmarcada"/>
+						</div>
+						<div>
+							<h3>
+								Gonzalo Guzmán del Río
+							</h3>
+							<p>
+								"Mi apellido lo dice todo, soy un buen hombre. Integrante del equipo de programación. Amante del fútbol, puede que ahora esté viendo un partido de la liga de Djibouti del año 2005, un saludo."
+							</p>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div>
+							<h3>
+								Borja Cano Álvarez
+							</h3>
+							<p>
+								"Integrante del equipo de programación. Hice el instalador y cambié el código entero. Sé tocar la guitarra y hago un pisto que te cagas. A Alex y Gonzalo no les gusta, un saludo."
+							</p>
+						</div>
+						<div class="image-class" id="PosImagen">
+							<img  src="./img/borja.jpeg" alt="Borja Foto" class="enmarcada"/>
+						</div>
+					</div>
+					<div class="divided-container">
+						<div class="image-class">
+							<img  src="./img/marcos.jpeg" alt="Marcos Foto" class="enmarcada"/>
+						</div>
+						<div>
+							<h3>
+								Marcos Llinares Montes
+							</h3>
+							<p>
+								"Canario en Madrid. Encargado de los fondos del juego y de los menús. Amante de los vapes, mi sabor favorito actualmente el de cacahuete, un saludo. "
+							</p>
+						</div>
+					</div>
+				</div>
+  		</section>
+
+  	</div>
+    <footer id="footer" class="correct-position">
+				<h4>Contáctanos</h4><!-- start Contact Box -->
+				<a class="footer-text">A "Serious-Train" Game. All Rights Reserved. © 2017 Serious-Train</a>
+				<script data-sil-id='592b1082a43fd60015db4f5f'> (function() {var d = document, w = window, l = window.location,p = l.protocol == 'file:' ? 'http://' : '//';if (!w.WS) w.WS = {}; c = w.WS;var m=function(t, o){  var e = d.getElementsByTagName('script'); e=e[e.length-1];  var n = d.createElement(t); if (t=='script') {n.async=true;} for (k in o) n[k] = o[k];  e.parentNode.insertBefore(n, e)};   m('script', {      src: p+'bawkbox.com/widget/contact/592b1082a43fd60015db4f5f?page='+encodeURIComponent(l+''),      type: 'text/javascript'  });  c.load_net = m;})();</script><div class='sil-widget-contact sil-widget' id='sil-widget-592b1082a43fd60015db4f5f'<a href='https://htmlcontactbox.com'>Contact Box is loading...</a></div>
+<!-- end Contact Box -->
+  	</footer>
+		<script type="text/javascript" src="./js/main.js"></script>
+	</body>
+</html>
